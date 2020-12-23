e4c6eab5a1888612f418029c7b89a898bbca4727
shwetabangera <shweta.bangera@raweng.com>
2020-12-23 16:55:44 +0530

    Question modified

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -4,7 +4,7 @@ function Instruction() {
 			<p>Time limit is: 5 Minutes</p>
 			<p>No Negative Marking</p>
 			<p>All Questions are compulsory</p>
-			<p>submit Questions</p>
+			<p>Submit Questions</p>
 		</div>
 	);
 }
